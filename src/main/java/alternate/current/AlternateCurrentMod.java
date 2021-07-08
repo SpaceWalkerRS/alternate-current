49e6ce650cef1b9adcf76d39e8fa04e619603776
Space Walker <spacewalkerrs@gmail.com>
2021-07-08 22:21:34 +0200

    Updated mod version to 0.2.0

--- src/main/java/alternate/current/AlternateCurrentMod.java
+++ src/main/java/alternate/current/AlternateCurrentMod.java
@@ -12,7 +12,7 @@ import net.fabricmc.api.ModInitializer;
 public class AlternateCurrentMod implements ModInitializer {
 	
 	public static final String MOD_ID = "alternate-current";
-	public static final String VERSION = "0.1.4";
+	public static final String VERSION = "0.2.0";
 	public static final Logger LOGGER = LogManager.getLogger("Alternate Current");
 	public static final boolean DEBUG = false;
 	
