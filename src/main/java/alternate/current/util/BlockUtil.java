02215c95988c14a2e884339237bcdc6524a9479b
Space Walker <spacewalkerrs@gmail.com>
2021-11-06 20:37:56 +0100

    Removed some unnecessary abstract modifiers

--- src/main/java/alternate/current/util/BlockUtil.java
+++ src/main/java/alternate/current/util/BlockUtil.java
@@ -2,7 +2,7 @@ package alternate.current.util;
 
 import net.minecraft.util.math.Direction;
 
-public abstract class BlockUtil {
+public class BlockUtil {
 	
 	/** Directions in the order in which they are used for emitting shape updates */
 	public static final Direction[] DIRECTIONS = { Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH, Direction.DOWN, Direction.UP };
