4fede1573080398cf541090c53a73e344ac82fcd
Space Walker <spacewalkerrs@gmail.com>
2021-11-06 20:38:52 +0100

    Removed some unnecessary abstract modifiers

--- src/main/java/alternate/current/mixin/RedstoneWireBlockMixin.java
+++ src/main/java/alternate/current/mixin/RedstoneWireBlockMixin.java
@@ -2,8 +2,8 @@ package alternate.current.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
