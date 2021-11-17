99afb087e7c08a80af8cb477e4860f2d6f6a9c92
Space Walker <spacedoesrs@gmail.com>
2021-11-17 19:08:07 +0100

    Improvements to connection manager and node cleanup, bump version to 1.0.0

--- src/main/java/alternate/current/util/BlockUtil.java
+++ src/main/java/alternate/current/util/BlockUtil.java
@@ -4,7 +4,9 @@ import net.minecraft.util.math.Direction;
 
 public class BlockUtil {
 	
-	/** Directions in the order in which they are used for emitting shape updates */
+	/** Directions in the order in which they are used for emitting shape updates. */
 	public static final Direction[] DIRECTIONS = { Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH, Direction.DOWN, Direction.UP };
+	
+	public static final int FLAG_NOTIFY_CLIENTS = 0b10;
 	
 }
