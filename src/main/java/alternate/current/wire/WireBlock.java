83ccf757b6d8215bfc6926c3f6175fb6a2cd1384
Space Walker <spacedoesrs@gmail.com>
2022-05-09 15:02:11 +0200

    move calls to WireHandler into WireBlock

--- src/main/java/alternate/current/wire/WireBlock.java
+++ src/main/java/alternate/current/wire/WireBlock.java
@@ -1,5 +1,11 @@
 package alternate.current.wire;
 
+import alternate.current.interfaces.mixin.IServerWorld;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
 /**
  * This interface should be implemented by any wire block type. While it is the
  * WireType class that represents different wire types and how they behave, this
@@ -12,4 +18,21 @@ public interface WireBlock {
 
 	public WireType getWireType();
 
+	default void onWireAdded(World world, BlockPos pos) {
+		if (!world.method_16390()) {
+			((IServerWorld)world).getWireHandler().onWireAdded(pos, getWireType());
+		}
+	}
+
+	default void onWireRemoved(World world, BlockPos pos, BlockState state) {
+		if (!world.method_16390()) {
+			((IServerWorld)world).getWireHandler().onWireRemoved(pos, state, getWireType());
+		}
+	}
+
+	default void onWireUpdated(World world, BlockPos pos) {
+		if (!world.method_16390()) {
+			((IServerWorld)world).getWireHandler().onWireUpdated(pos, getWireType());
+		}
+	}
 }
