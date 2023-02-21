d0aece5e8b593dbce1ed992234e4cfdb747aded1
Space Walker <spacedoesrs@gmail.com>
2023-02-21 16:02:39 +0100

    bump version to 1.5.0

--- src/main/java/alternate/current/AlternateCurrentMod.java
+++ src/main/java/alternate/current/AlternateCurrentMod.java
@@ -12,7 +12,7 @@ public class AlternateCurrentMod implements ModInitializer {
 
 	public static final String MOD_ID = "alternate-current";
 	public static final String MOD_NAME = "Alternate Current";
-	public static final String MOD_VERSION = "1.4.0";
+	public static final String MOD_VERSION = "1.5.0";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 	public static final boolean DEBUG = false;
 
