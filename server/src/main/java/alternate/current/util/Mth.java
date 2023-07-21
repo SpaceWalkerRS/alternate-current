123011bd64b74b2e56e094cb9cf878f22240278b
Space Walker <spacedoesrs@gmail.com>
2023-07-21 16:52:37 +0200

    port to mc a1.0.6

--- server/src/main/java/alternate/current/util/Mth.java
+++
@@ -1,8 +0,0 @@
-package alternate.current.util;
-
-public class Mth {
-
-	public static int clamp(int i, int min, int max) {
-		return i <= min ? min : (i >= max ? max : i);
-	}
-}
--- server/src/main/java/alternate/current/util/Redstone.java
+++
@@ -1,9 +0,0 @@
-package alternate.current.util;
-
-public class Redstone {
-
-	public static final int SIGNAL_MIN = 0;
-	public static final int SIGNAL_MAX = 15;
-	public static final int SIGNAL_STEP = 1;
-
-}
