af8ed01a1b7967b5ba1f45e1f4f1172abf98a632
Space Walker <spacedoesrs@gmail.com>
2024-05-06 15:15:58 +0200

    implement different update orders

--- src/main/java/alternate/current/command/AlternateCurrentCommand.java
+++ src/main/java/alternate/current/command/AlternateCurrentCommand.java
@@ -1,13 +1,22 @@
 package alternate.current.command;
 
-import alternate.current.AlternateCurrentMod;
-import alternate.current.util.profiler.ProfilerResults;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import alternate.current.AlternateCurrentMod;
+import alternate.current.interfaces.mixin.IServerWorld;
+import alternate.current.util.DimensionUtil;
+import alternate.current.util.profiler.ProfilerResults;
+import alternate.current.wire.UpdateOrder;
+import alternate.current.wire.WireHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.Command;
-import net.minecraft.server.command.ICommand;
 import net.minecraft.server.command.exception.CommandException;
 import net.minecraft.server.command.exception.IncorrectUsageException;
 import net.minecraft.server.command.source.CommandSource;
+import net.minecraft.world.World;
 
 public class AlternateCurrentCommand extends Command {
 
@@ -23,7 +32,7 @@ public class AlternateCurrentCommand extends Command {
 
 	@Override
 	public String getUsage(CommandSource source) {
-		return AlternateCurrentMod.DEBUG ? "/alternatecurrent [on/off/resetProfiler]" : "/alternatecurrent [on/off]";
+		return AlternateCurrentMod.DEBUG ? "/alternatecurrent [on/off/updateOrder[ updateOrder]/resetProfiler]" : "/alternatecurrent [on/off/updateOrder[ updateOrder]]";
 	}
 
 	@Override
@@ -33,14 +42,15 @@ public class AlternateCurrentCommand extends Command {
 			query(source);
 			return;
 		case 1:
-			String arg = args[0];
-
-			switch (arg) {
+			switch (args[0]) {
 			case "on":
 				set(source, true);
 				return;
 			case "off":
 				set(source, false);
+				return;
+			case "updateOrder":
+				queryUpdateOrder(source);
 				return;
 			case "resetProfiler":
 				if (AlternateCurrentMod.DEBUG) {
@@ -54,9 +64,52 @@ public class AlternateCurrentCommand extends Command {
 			}
 
 			break;
+		case 2:
+			switch (args[0]) {
+			case "updateOrder":
+				for (UpdateOrder updateOrder : UpdateOrder.values()) {
+					if (args[1].equals(updateOrder.id())) {
+						setUpdateOrder(source, updateOrder);
+						return;
+					}
+				}
+			}
 		}
 
 		throw new IncorrectUsageException(getUsage(source));
+	}
+
+	@Override
+	public List<String> getSuggestions(CommandSource source, String[] args) {
+		switch (args.length) {
+		case 0:
+			return AlternateCurrentMod.DEBUG
+				? suggestMatching(args, "on", "off", "updateOrder", "resetProfiler")
+				: suggestMatching(args, "on", "off", "updateOrder");
+		case 1:
+			switch (args[0]) {
+			case "updateOrder":
+				List<String> updateOrders = new ArrayList<>();
+
+				for (UpdateOrder updateOrder : UpdateOrder.values()) {
+					updateOrders.add(updateOrder.id());
+				}
+
+				return suggestMatching(args, updateOrders);
+			}
+		}
+
+		return Collections.emptyList();
+	}
+
+	private static World getWorld(CommandSource source) {
+		if (source instanceof MinecraftServer) {
+			return ((MinecraftServer)source).getWorld(DimensionUtil.OVERWORLD);
+		}
+		if (source instanceof Entity) {
+			return ((Entity)source).world;
+		}
+		return null;
 	}
 
 	private void query(CommandSource source) {
@@ -71,8 +124,26 @@ public class AlternateCurrentCommand extends Command {
 		sendSuccess(source, String.format("Alternate Current has been %s!", state));
 	}
 
+	private void queryUpdateOrder(CommandSource source) {
+		World world = getWorld(source);
+		WireHandler wireHandler = ((IServerWorld) world).alternate_current$getWireHandler();
+
+		String value = wireHandler.getConfig().getUpdateOrder().id();
+		source.sendMessage(String.format("Update order is currently %s", value));
+	}
+
+	private void setUpdateOrder(CommandSource source, UpdateOrder updateOrder) {
+		World world = getWorld(source);
+		WireHandler wireHandler = ((IServerWorld) world).alternate_current$getWireHandler();
+
+		wireHandler.getConfig().setUpdateOrder(updateOrder);
+
+		String value = wireHandler.getConfig().getUpdateOrder().id();
+		sendSuccess(source, String.format("update order has been set to %s!", value));
+	}
+
 	@Override
 	public int compareTo(Object o) {
-		return super.compareTo((ICommand)o);
+		return super.compareTo((Command) o);
 	}
 }
---
+++ src/main/java/alternate/current/util/DimensionUtil.java
@@ -0,0 +1,7 @@
+package alternate.current.util;
+
+public class DimensionUtil {
+
+	public static final int OVERWORLD = 0;
+
+}
