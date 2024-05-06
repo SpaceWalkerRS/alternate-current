2254a13fad52e9d1c2608c148c5516c873aa1224
Space Walker <spacedoesrs@gmail.com>
2024-05-06 15:15:58 +0200

    implement different update orders

--- src/main/java/alternate/current/AlternateCurrentMod.java
+++ src/main/java/alternate/current/AlternateCurrentMod.java
@@ -1,7 +1,7 @@
 package alternate.current;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import alternate.current.util.profiler.ACProfiler;
 import alternate.current.util.profiler.Profiler;
@@ -11,7 +11,7 @@ public class AlternateCurrentMod {
 	public static final String MOD_ID = "alternate-current";
 	public static final String MOD_NAME = "Alternate Current";
 	public static final String MOD_VERSION = "1.7.0";
-	public static final Logger LOGGER = LogManager.getLogManager().getLogger(MOD_NAME);
+	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 	public static final boolean DEBUG = false;
 
 	public static boolean on = true;
