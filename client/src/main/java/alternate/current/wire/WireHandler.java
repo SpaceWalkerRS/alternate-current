b3f6bcb502fd848aa80785a3b62e7db512fb4680
Space Walker <spacedoesrs@gmail.com>
2024-08-24 11:24:36 +0200

    take advantage of blockstate caching more

--- client/src/main/java/alternate/current/wire/WireHandler.java
+++ client/src/main/java/alternate/current/wire/WireHandler.java
@@ -956,7 +956,20 @@ public class WireHandler {
 	 */
 	private void queueNeighbor(Node node, WireNode neighborWire) {
 		// Updates to wires are queued when power is transmitted.
-		if (!node.isWire()) {
+		// While this check makes sure wires in the network are not given block
+		// updates, it also prevents block updates to wires in neighboring networks.
+		// While this should not make a difference in theory, in practice, it is
+		// possible to force a network into an invalid state without updating it, even
+		// if it is relatively obscure.
+		// While I was willing to make this compromise in return for some significant
+		// performance gains in certain setups, if you are not, you can add all the
+		// positions of the network to a set and filter out block updates to wires in
+		// the network that way.
+		// Block updates to air do nothing, so those are skipped as well.
+		// The current block state at this position *could* be wrong, but if you somehow
+		// manage to place a block where air used to be during the execution of a block
+		// update I am very impressed and you deserve to have some broken behavior.
+		if (!node.isWire() && !node.state.isAir()) {
 			node.neighborWire = neighborWire;
 			updates.offer(node);
 		}
@@ -982,19 +995,7 @@ public class WireHandler {
 	private void updateBlock(Node node, Block neighborBlock) {
 		BlockPos pos = node.pos;
 		BlockState state = WorldHelper.getBlockState(world, pos);
-
-		// While this check makes sure wires in the network are not given block
-		// updates, it also prevents block updates to wires in neighboring networks.
-		// While this should not make a difference in theory, in practice, it is
-		// possible to force a network into an invalid state without updating it, even
-		// if it is relatively obscure.
-		// While I was willing to make this compromise in return for some significant
-		// performance gains in certain setups, if you are not, you can add all the
-		// positions of the network to a set and filter out block updates to wires in
-		// the network that way.
-		if (!state.isAir() && !state.is(Block.REDSTONE_WIRE)) {
-			state.neighborChanged(world, pos, neighborBlock);
-		}
+		state.neighborChanged(world, pos, neighborBlock);
 	}
 
 	@FunctionalInterface
